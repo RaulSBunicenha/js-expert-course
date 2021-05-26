cf9164c59419bc03f98a9ef848538b1bed29210d
Raul B <raulbunicenha@gmail.com>
2021-05-26 14:36:28 -0300

    toString valueOf and Symbol.toPrimitive

--- mod02-live-cicle-01/demo02-type-coercion/index.js
+++ mod02-live-cicle-01/demo02-type-coercion/index.js
@@ -49,3 +49,46 @@
     console.assert(('hello' && 123) === 123, '&& returns the first element, when both are true, first test')
     console.assert((!'hello' && 123) === false, '&& returns false when one of them is false')
 }
+
+
+{
+    const item = {
+        name: 'Raul',
+        age: 25
+    }
+
+    console.assert(item + 1 === '[object Object]1', 'Do not have toString or valueOf function to Call')
+    // 
+
+    // Call first when trying to convert to string, if do not return a primitive value, call valueOf
+    item.toString = function () {
+        return `| Name: ${this.name} - Age:${this.age} |`
+    }
+
+    console.assert(item + 1 === '| Name: Raul - Age:25 |1', 'When is trying to convert to string call toString, if do not return a primitive value, call valueOf')
+    // 
+
+    // Call first when trying to convert to number, if do not return a primitive value, call toString
+    item.valueOf = function () {
+        return this.name.length + this.age
+    }
+
+    console.assert(item + 1 === 30, 'When is trying to convert to number call valueOf, if do not return a primitive value, call toString')
+    
+    item.valueOf = function () {
+        return { message: 'This is a new object' }
+    }
+    
+    console.assert(Boolean(Number(item)) === false, 'Try to convert to number, but the valueOf returns an non-primitive value and toString returns a string, so it returns a NaN')
+
+    // Symbol To Primitive is called first than all of others coercion functions
+    item[Symbol.toPrimitive] = function (coercionType) {
+        return {
+            string: JSON.stringify(this),
+            number: this.name.length + this.age
+        }[coercionType]
+    }
+
+    console.assert(String(item) === '{"name":"Raul","age":25}', 'Call Symbol.toPrimitive function when is trying to convert to string')
+    console.assert(Number(item) === 29, 'Call Symbol.toPrimitive function when is trying to convert to number')
+}
