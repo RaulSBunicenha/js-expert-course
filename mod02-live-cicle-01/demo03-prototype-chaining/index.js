cb2adf551e6dc1e164a9f5ed956e410eaf11acca
Raul B <raulbunicenha@gmail.com>
2021-05-31 18:06:45 -0300

    Class Sintaxe Proto Study

--- mod02-live-cicle-01/demo03-prototype-chaining/index.js
+++ mod02-live-cicle-01/demo03-prototype-chaining/index.js
@@ -73,3 +73,30 @@ const { deepStrictEqual } = require('assert')
 
     console.log('----------')
 }
+
+{
+    class T1 {
+        ping () { return 'ping' }
+    }
+
+    class T2 extends T1 {
+        pong () { return 'pong' }
+    }
+
+    class T3 extends T2 {
+        shoot () { return 'shoot' }
+    }
+
+    const t3 = new T3()
+
+    console.log('t3 inherits null?', t3.__proto__.__proto__.__proto__.__proto__.__proto__ === null)
+    console.log('t3.ping()', t3.ping())
+    console.log('t3.pong()', t3.pong())
+    console.log('t3.shoot()', t3.shoot())
+
+    deepStrictEqual(t3.__proto__, T3.prototype)
+    deepStrictEqual(t3.__proto__.__proto__, T2.prototype)
+    deepStrictEqual(t3.__proto__.__proto__.__proto__, T1.prototype)
+    deepStrictEqual(t3.__proto__.__proto__.__proto__.__proto__, Object.prototype)
+    deepStrictEqual(t3.__proto__.__proto__.__proto__.__proto__.__proto__, null)
+}
