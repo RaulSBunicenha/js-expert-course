7ac4534cf0c3809e77acc2d31aab42d6d23e060c
Raul B <raulbunicenha@gmail.com>
2021-06-02 15:14:27 -0300

    Show Database using DraftLog and Chalk

--- mod03-live-cycle-02/demo02-esmodules-internacionalization/src/index.js
+++ mod03-live-cycle-02/demo02-esmodules-internacionalization/src/index.js
@@ -1,4 +1,28 @@
+import DraftLog from 'draftlog'
+import chalk from 'chalk'
+import chalkTable from 'chalk-table'
+
 import database from '../database.json'
 
-console.log('database', database)
+DraftLog(console).addLineListener(process.stdin)
 
+const options = {
+  leftPad: 2,
+  columns: [
+    { field: "id", name: chalk.cyan("ID") },
+    { field: "vehicles", name: chalk.cyan("Vehicles") },
+    { field: "kmTraveled", name: chalk.cyan("Km Traveled") },
+    { field: "from", name: chalk.cyan("From") },
+    { field: "to", name: chalk.cyan("To") }
+  ]
+}
+
+const table = chalkTable(options, database)
+
+const print = console.draft(table)
+
+setInterval(() => {
+  database.push({ id: "rualsa", "vehicles": ['Item 1', 'Item 2'] })
+  const table = chalkTable(options, database)
+  print(table)
+}, 3000)
